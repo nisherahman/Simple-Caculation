9b0eec238bca8dd0cb24b6fe7099b8d566c17ab8
nisherahman <frozennishe@gmail.com>
2021-10-08 18:18:07 +0600

    js practice

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+//Arithmetic Operators
+
+// addition
+function addition()
+{
+    var num1 = 300;
+    var num2 = 55;
+    var result = num1 + num2;
+    document.getElementById("one").innerHTML = result;
+}
+
+//subtraction
+function subtraction()
+{
+    var num1 = 450;
+    var num2 = 200;
+    var result = num1 - num2;
+    document.getElementById("two").innerHTML = result;
+}
+
+//Multiplication
+function multiplication()
+{
+    var num1 = 600;
+    var num2 = 40;
+    var result = num1 * num2;
+    document.getElementById("three").innerHTML = result;
+}
+
+//division
+function division()
+{
+    var num1 = 500;
+    var num2 = 20;
+    var result = num1 / num2;
+    document.getElementById("four").innerHTML = result;
+}
+
+//mod
+function percent()
+{
+    var num1 = 300;
+    var num2 = 50;
+    var result = num1  % num2;
+    document.getElementById("five").innerHTML = result;
+}
